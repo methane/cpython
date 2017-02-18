0e4f324b08a9822bd327a1d41301452f903c5c11
INADA Naoki <songofacandy@gmail.com>
2017-02-18 18:59:25 +0900

    float uses global freelist

--- Objects/floatobject.c
+++ Objects/floatobject.c
@@ -10,17 +10,6 @@
 #include <float.h>
 
 
-/* Special free list
-   free_list is a singly-linked list of available PyFloatObjects, linked
-   via abuse of their ob_type members.
-*/
-
-#ifndef PyFloat_MAXFREELIST
-#define PyFloat_MAXFREELIST    100
-#endif
-static int numfree = 0;
-static PyFloatObject *free_list = NULL;
-
 double
 PyFloat_GetMax(void)
 {
@@ -109,14 +98,10 @@ PyFloat_GetInfo(void)
 PyObject *
 PyFloat_FromDouble(double fval)
 {
-    PyFloatObject *op = free_list;
-    if (op != NULL) {
-        free_list = (PyFloatObject *) Py_TYPE(op);
-        numfree--;
-    } else {
-        op = (PyFloatObject*) PyObject_MALLOC(sizeof(PyFloatObject));
-        if (!op)
-            return PyErr_NoMemory();
+    PyFloatObject *op;
+    op = (PyFloatObject*) _PyFreelist_Malloc(sizeof(PyFloatObject));
+    if (!op) {
+        return PyErr_NoMemory();
     }
     /* Inline PyObject_New */
     (void)PyObject_INIT(op, &PyFloat_Type);
@@ -212,13 +197,7 @@ static void
 float_dealloc(PyFloatObject *op)
 {
     if (PyFloat_CheckExact(op)) {
-        if (numfree >= PyFloat_MAXFREELIST)  {
-            PyObject_FREE(op);
-            return;
-        }
-        numfree++;
-        Py_TYPE(op) = (struct _typeobject *)free_list;
-        free_list = op;
+        _PyFreelist_Free(op, sizeof(PyFloatObject));
     }
     else
         Py_TYPE(op)->tp_free((PyObject *)op);
@@ -1959,16 +1938,7 @@ _PyFloat_Init(void)
 int
 PyFloat_ClearFreeList(void)
 {
-    PyFloatObject *f = free_list, *next;
-    int i = numfree;
-    while (f) {
-        next = (PyFloatObject*) Py_TYPE(f);
-        PyObject_FREE(f);
-        f = next;
-    }
-    free_list = NULL;
-    numfree = 0;
-    return i;
+    return 0;
 }
 
 void
@@ -1983,7 +1953,7 @@ _PyFloat_DebugMallocStats(FILE *out)
 {
     _PyDebugAllocatorStats(out,
                            "free PyFloatObject",
-                           numfree, sizeof(PyFloatObject));
+                           0, sizeof(PyFloatObject));
 }
 
 
